403b12adaff84d21d25301b481d19fc550692782
Sam Russell <sam.h.russell@gmail.com>
2013-08-15 16:41:32 +1200

    got it faulting all the time - problem is that it sees there is a valid h264 encoder but it loads the neutered codec instead, then tries to call the non-existent .encode function and shits its pants

--- transcode01.c
+++ transcode01.c
@@ -27,20 +27,20 @@ void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame) {
   FILE *pFile;
   char szFilename[32];
   int  y;
-  
+
   // Open file
   sprintf(szFilename, "frame%d.ppm", iFrame);
   pFile=fopen(szFilename, "wb");
   if(pFile==NULL)
     return;
-  
+
   // Write header
   fprintf(pFile, "P6\n%d %d\n255\n", width, height);
-  
+
   // Write pixel data
   for(y=0; y<height; y++)
     fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-  
+
   // Close file
   fclose(pFile);
 }
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
   int             i, videoStream;
   AVCodecContext  *pCodecCtx = NULL;
   AVCodec         *pCodec = NULL;
-  AVFrame         *pFrame = NULL; 
+  AVFrame         *pFrame = NULL;
   AVFrame         *pFrameRGB = NULL;
   AVPacket        packet;
   int             frameFinished;
@@ -59,41 +59,44 @@ int main(int argc, char *argv[]) {
 
   AVDictionary    *optionsDict = NULL;
   struct SwsContext      *sws_ctx = NULL;
-  
+
   AVFormatContext *pFormatCtxOut = NULL;
   int             videoStreamOut;
   AVCodecContext  *pCodecCtxOut = NULL;
   AVCodec         *pCodecOut = NULL;
-  AVFrame         *pFrameOut = NULL; 
+  AVFrame         *pFrameOut = NULL;
   AVPacket        packetOut;
   int             frameFinishedOut;
   FILE*           f=NULL;
   int             ret;
   int             got_output;
-  
+  int             out_size;
+  int             outbuf_size;
+  uint8_t         *outbuf;
+
   uint8_t endcode[] = { 0, 0, 1, 0xb7 };
 
   AVDictionary    *optionsDictOut = NULL;
   struct SwsContext *sws_ctxOut = NULL;
-  
+
   if(argc < 3) {
     printf("Please provide a movie file\n");
     return -1;
   }
   // Register all formats and codecs
   av_register_all();
-  
+
   // Open video file
   if(avformat_open_input(&pFormatCtx, argv[1], NULL, NULL)!=0)
     return -1; // Couldn't open file
-  
+
   // Retrieve stream information
   if(avformat_find_stream_info(pFormatCtx, NULL)<0)
     return -1; // Couldn't find stream information
-  
+
   // Dump information about file onto standard error
   av_dump_format(pFormatCtx, 0, argv[1], 0);
-  
+
   // Find the first video stream
   videoStream=-1;
   for(i=0; i<pFormatCtx->nb_streams; i++)
@@ -103,10 +106,10 @@ int main(int argc, char *argv[]) {
     }
   if(videoStream==-1)
     return -1; // Didn't find a video stream
-  
+
   // Get a pointer to the codec context for the video stream
   pCodecCtx=pFormatCtx->streams[videoStream]->codec;
-  
+
   // Find the decoder for the video stream
   pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
   if(pCodec==NULL) {
@@ -116,15 +119,15 @@ int main(int argc, char *argv[]) {
   // Open codec
   if(avcodec_open2(pCodecCtx, pCodec, &optionsDict)<0)
     return -1; // Could not open codec
-  
+
   // Allocate video frame
   pFrame=avcodec_alloc_frame();
-  
+
   // Allocate an AVFrame structure
   pFrameRGB=avcodec_alloc_frame();
   if(pFrameRGB==NULL)
     return -1;
-  
+
   // Determine required buffer size and allocate buffer
   numBytes=avpicture_get_size(PIX_FMT_RGB24, pCodecCtx->width,
 			      pCodecCtx->height);
@@ -144,29 +147,29 @@ int main(int argc, char *argv[]) {
         NULL,
         NULL
     );
-  
+
   // Assign appropriate parts of buffer to image planes in pFrameRGB
   // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
   // of AVPicture
   avpicture_fill((AVPicture *)pFrameRGB, buffer, PIX_FMT_RGB24,
 		 pCodecCtx->width, pCodecCtx->height);
-  
+
   // Set up output encoder too
-  
+
   // find the encoder AV_CODEC_ID_H264
   pCodecOut = avcodec_find_encoder(CODEC_ID_H264);
-  
+
   if (!pCodecOut) {
     fprintf(stderr, "Codec not found\n");
     exit(1);
   }
-  
+
   pCodecCtxOut = avcodec_alloc_context3(pCodecOut);
   if (!pCodecCtxOut) {
     fprintf(stderr, "Could not allocate video codec context\n");
     exit(1);
   }
-  
+
   // put sample parameters
   /*
   c->bit_rate = 400000;
@@ -179,30 +182,30 @@ int main(int argc, char *argv[]) {
   c->max_b_frames=1;
   c->pix_fmt = AV_PIX_FMT_YUV420P;
   */
-  
+
   // steal parameters from the other context
   pCodecCtxOut->bit_rate = pCodecCtx->bit_rate;
-  
+
   pCodecCtxOut->width = pCodecCtx->width;
   pCodecCtxOut->height = pCodecCtx->height;
   pCodecCtxOut->time_base = pCodecCtx->time_base;
   pCodecCtxOut->gop_size = pCodecCtx->gop_size;
   pCodecCtxOut->max_b_frames = pCodecCtx->max_b_frames;
   pCodecCtxOut->pix_fmt = pCodecCtx->pix_fmt;
-  
+
   if (avcodec_open2(pCodecCtxOut, pCodecOut, NULL) < 0) {
     fprintf(stderr, "Could not open codec\n");
     exit(1);
   }
-  
-  
- 
+
+
+
   f = fopen(argv[2], "wb");
   if (!f) {
     fprintf(stderr, "Could not open %s\n", argv[2]);
     exit(1);
   }
- 
+
   //pFrameOut = avcodec_alloc_frame();
   //if (!pFrameOut) {
   //  fprintf(stderr, "Could not allocate video frame\n");
@@ -211,7 +214,7 @@ int main(int argc, char *argv[]) {
   //pFrameOut->format = pCodecCtxOut->pix_fmt;
   //pFrameOut->width  = pCodecCtxOut->width;
   //pFrameOut->height = pCodecCtxOut->height;
-  
+
   /* the image can be allocated by any means and av_image_alloc() is
    * just the most convenient way if av_malloc() is to be used */
   //ret = av_image_alloc(frame->data, frame->linesize, c->width, c->height,
@@ -233,7 +236,7 @@ int main(int argc, char *argv[]) {
       fprintf(stderr, "Error encoding frame\n");
       exit(1);
     }
- 
+
     if (got_output) {
       printf("Write frame %3d (size=%5d)\n", i, packetOut.size);
       fwrite(packetOut.data, 1, packetOut.size, f);
@@ -241,7 +244,7 @@ int main(int argc, char *argv[]) {
     }
   */
   //}
- 
+
   /* get the delayed frames */
   /*
   for (got_output = 1; got_output; i++) {
@@ -252,7 +255,7 @@ int main(int argc, char *argv[]) {
       fprintf(stderr, "Error encoding frame\n");
       exit(1);
     }
- 
+
     if (got_output) {
       printf("Write frame %3d (size=%5d)\n", i, packetOut.size);
       fwrite(packetOut.data, 1, packetOut.size, f);
@@ -260,7 +263,7 @@ int main(int argc, char *argv[]) {
     }
   }
   */
- 
+
   /*
   fwrite(endcode, 1, sizeof(endcode), f);
   fclose(f);
@@ -271,18 +274,28 @@ int main(int argc, char *argv[]) {
   //avcodec_free_frame(&frame);
   printf("\n");
   */
-  
+
   // Do the transcode part
-  
+
+  // Prepare buffer for encoded data
+
+
+
+outbuf_size=100000;
+outbuf=calloc(outbuf_size, 1);
+printf("Buffer address: 0x%X\n", outbuf);
+
+printf("Ready to start the process\n");
+
   // Read frames and save first five frames to disk
   i=0;
   while(av_read_frame(pFormatCtx, &packet)>=0) {
     // Is this a packet from the video stream?
     if(packet.stream_index==videoStream) {
       // Decode video frame
-      avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, 
+      avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished,
 			   &packet);
-      
+
       // Did we get a video frame?
       if(frameFinished) {
 	  // Convert the image from its native format to RGB
@@ -296,69 +309,82 @@ int main(int argc, char *argv[]) {
             pFrameRGB->data,
             pFrameRGB->linesize
         );
-        
-    
+
+
         // also encode
-        av_init_packet(&packetOut);
+        //av_init_packet(&packetOut);
         packetOut.data = NULL;    // packet data will be allocated by the encoder
         packetOut.size = 0;
-    
+
+
         // encode the image
-        ret = avcodec_encode_video2(pCodecCtxOut, &packetOut, pFrame, &got_output);
-        if (ret < 0) {
-          fprintf(stderr, "Error encoding frame\n");
-          exit(1);
-        }
-     
-        if (got_output) {
-          printf("Write frame %3d (size=%5d)\n", i, packetOut.size);
-          fwrite(packetOut.data, 1, packetOut.size, f);
-          av_free_packet(&packetOut);
-        }
+        //ret = avcodec_encode_video2(pCodecCtxOut, &packetOut, pFrame, &got_output);
+        //if (ret < 0) {
+        //  fprintf(stderr, "Error encoding frame\n");
+        //  exit(1);
+        //}
+
+        printf("About to start encoding\n");
+
+        // use avcodec_encode_video() (not 2)
+        out_size = avcodec_encode_video(pCodecCtx, outbuf, outbuf_size, pFrame);
+        printf("Called avcodec_encode_video()\n");
+        printf("encoding frame %3d (size=%5d)\n", i, out_size);
+        fwrite(outbuf, 1, out_size, f);
+
+        //if (got_output) {
+        //  printf("Write frame %3d (size=%5d)\n", i, packetOut.size);
+        //  fwrite(packetOut.data, 1, packetOut.size, f);
+        //  av_free_packet(&packetOut);
+        //}
 	    // Save the frame to disk
-	    if(++i<=5)
+	    if(++i<=50)
 	      SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height, i);
       }
     }
-    
+
     // Free the packet that was allocated by av_read_frame
     av_free_packet(&packet);
   }
-  
+
   // finish encode
-  for (got_output = 1; got_output; i++) {
+  for (out_size=1; out_size; i++) {
     fflush(stdout);
 
-    ret = avcodec_encode_video2(pCodecCtxOut, &packetOut, NULL, &got_output);
-    if (ret < 0) {
-      fprintf(stderr, "Error encoding frame\n");
-      exit(1);
-    }
- 
-    if (got_output) {
-      printf("Write frame %3d (size=%5d)\n", i, packetOut.size);
-      fwrite(packetOut.data, 1, packetOut.size, f);
-      av_free_packet(&packetOut);
-    }
+    //ret = avcodec_encode_video2(pCodecCtxOut, &packetOut, NULL, &got_output);
+    //if (ret < 0) {
+    //  fprintf(stderr, "Error encoding frame\n");
+    //  exit(1);
+    //}
+
+    //if (got_output) {
+    //  printf("Write frame %3d (size=%5d)\n", i, packetOut.size);
+    //  fwrite(packetOut.data, 1, packetOut.size, f);
+    //  av_free_packet(&packetOut);
+    //}
+    out_size = avcodec_encode_video(pCodecCtx, outbuf, outbuf_size, NULL);
+    printf("encoding frame %3d (size=%5d)\n", i, out_size);
+    fwrite(outbuf, 1, out_size, f);
+
   }
-  
+
   /* add sequence end code to have a real mpeg file */
   fwrite(endcode, 1, sizeof(endcode), f);
   fclose(f);
   avcodec_close(pCodecCtxOut);
-  
+
   // Free the RGB image
   av_free(buffer);
   av_free(pFrameRGB);
-  
+
   // Free the YUV frame
   av_free(pFrame);
-  
+
   // Close the codec
   avcodec_close(pCodecCtx);
-  
+
   // Close the video file
   avformat_close_input(&pFormatCtx);
-  
+
   return 0;
 }
